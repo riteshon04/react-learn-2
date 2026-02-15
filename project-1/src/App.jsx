2a66def9c9707f059aa22bf14e2d7548dc2fd055
{krishnaon1987-creator} <{krishnaon1987@gmail.com}>
2026-02-15 12:06:44 +0530

    this is my first day to learn the react

---
+++ project-1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='dad'>
+      <h1>Hii Ritesh singh</h1>
+    </div>
+    <div id="chacha">hello chacha</div>
+   
+
+    </>
+  )
+}
+
+export default App
